d5edd2c3026d53d598a7b48ff1bc824384dfb919
SchiggyWarra <49441366+Paulusk@users.noreply.github.com>
2022-10-04 13:57:48 +0200

    finale changes

--- index.js
+++ index.js
@@ -5,9 +5,25 @@ const clear = document.getElementById("clearStorage")
 let array = ["kiol"]
 const inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
+const saveBtn = document.getElementById("save-btn")
+
 
 //first i need to see, if there is already content within in localstorage
 //1.
+
+const tab = [{url:"wwww.google.de"}]
+console.log(tab[0].url)
+
+saveBtn.addEventListener("click", function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    array.push(tabs[0].url)
+    setStorage(array)
+    render(array)
+
+    })
+  
+})
 
 if(localStorage.length === 0 ) {
     array = []
@@ -18,7 +34,7 @@ if(localStorage.length === 0 ) {
 
 
 function render(leads) {
-
+ulEl.innerHTML = ""
     for(i=0; i<leads.length; i++) {
         ulEl.innerHTML += `
         <li>
@@ -29,6 +45,10 @@ function render(leads) {
         `
     }
 }
+function setStorage(entry) {
+    localStorage.clear()
+    localStorage.setItem("leads", JSON.stringify(entry))
+}
 
 //2. - then, i need to copy that content into an array, or create an empty array
 
@@ -36,9 +56,7 @@ newEntry.addEventListener("click", function() {
     if(inputEl.value != 0){
     array.push(inputEl.value)
     inputEl.value = ""
-    localStorage.clear()
-    localStorage.setItem("leads", JSON.stringify(array))
-    ulEl.innerHTML = ""
+    setStorage(array)
     render(array)
     }else{}
     })
